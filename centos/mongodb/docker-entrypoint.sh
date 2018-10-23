5a586f7639bd53d945255aa026f4354b8ab9da18
weihoop <liuhongwei@bd-yg.com>
2018-10-23 17:14:10 +0800

    update

--- centos/mongodb/docker-entrypoint.sh
+++ centos/mongodb/docker-entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+set -e 
+
+chown mongodb:mongodb -R /data/ 
+
+gosu mongodb bash -c "mongod -f /etc/mongod.conf"
 
 #gosu mongodb bash -c "/usr/local/mongodb/bin/mongod -f /etc/mongod.conf"
-gosu mongodb bash -c "mongod -f /etc/mongod.conf"
-#su - mongodb -s /bin/bash -c "/usr/local/mongodb/bin/mongod -f /etc/mongod.conf"
